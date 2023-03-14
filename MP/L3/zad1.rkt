a80e51de30485b05857de5cbc695695ce2b98620
Jeremiasz Preiss <80748387+pan-kuleczka@users.noreply.github.com>
2023-03-14 15:22:14 +0100

    Update zad1.rkt

--- MP/L3/zad1.rkt
+++ MP/L3/zad1.rkt
@@ -1,9 +1,9 @@
 #lang racket
 '(( car ( a . b)) (* 2) )
-(list (list car (cons 'a 'b)) (list * 2))
+(list (list car (cons 'a 'b)) (list '* 2))
 
 `( ,( car '(a . b)) ,(* 2) )
-(list (car (cons 'a 'b)) (* 2))
+(list (car (cons 'a 'b)) ('* 2))
 
 '((+ 1 2 3) ( cons ) ( cons a b))
-(list (list + 1 2 3) (list cons) (list cons 'a 'b))
+(list (list '+ 1 2 3) (list 'cons) (list 'cons 'a 'b))
